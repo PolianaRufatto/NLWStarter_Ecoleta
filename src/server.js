3cb1b3be882b3959c426b9939bcb13ed5263f71e
Poliana Rufatto <poliana.rufatto@softplan.com.br>
2020-08-06 05:49:24 -0300

    Criação do banco de dados

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 const express = require("express")
 const server = express()
 
+// pegar o banco de dados
+const db = require("./database/db")
+
 // Configurar pasta "public"
 server.use(express.static("public"))
 
+// uso do req.body
+server.use(express.urlencoded({ extended: true }))
 
 // Utilizando template engine
 const nunjucks = require("nunjucks")
@@ -21,9 +26,61 @@ server.get("/create-point", (req, res) => {
     return res.render("create-point.html")
 })
 
+server.post("/savepoint", (req, res) => {
+
+
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items 
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if(err) return console.log(err);
+
+        console.log("Cadastrado com Sucesso");
+        console.log(this)
+
+        return res.render("create-point.html", { saved: true })
+    }
+
+    db.run(query, values, afterInsertData);
+})
+
 server.get("/search-results", (req, res) => {
-    return res.render("search-results.html")
+
+    const search = req.query.search
+
+    if (search == "") {
+        return res.render("search-results.html", { total: 0 })
+    }
+
+    // pegar os dados do db
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
+        if(err) {
+            console.log(err)
+            return res.send("Erro no cadastro!")
+        }
+        const total = rows.length
+
+        return res.render("search-results.html", { places: rows, total })
+    })
 })
 
 //ligar o servidor
-server.listen(3000)
+server.listen(4000)
